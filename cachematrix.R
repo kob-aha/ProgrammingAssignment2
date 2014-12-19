02abf001b0a206a91b0377d4ca9c58a148f99477
Koby Aharon <kobyahron@gmail.com>
2014-12-19 20:34:59 +0200

    Implement programming assignment functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions calculates the inverse of a matrix.
+## Matrix iversion is usually costly so we will create a caching mechanism to store the inverse matrix and 
+## recalculate it only when the matrix changes. 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## The following function creates a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(mat = matrix()) {
+  inversemat <- NULL
+  setmat <- function(newmat) {
+    mat <<- newmat
+    inversemat <<- NULL
+  }
+  getmat <- function() mat
+  setimat <- function(imat) inversemat <<- imat
+  getimat <- function() inversemat
+  list(setmat = setmat, 
+       getmat = getmat,
+       setimat = setimat,
+       getimat = getimat)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## The following function computes the inverse of a matrix returned by "makeCacheMatrix". 
+## It uses "makeCacheMatrix" caching ability and make sure to calculate the inverse matrix only once.
+cacheSolve <- function(mat, ...) {
+  imat <- mat$getimat()
+  if(!is.null(imat)) {
+    message("getting cached data")
+    return(imat)
+  }
+  datamat <- mat$getmat()
+  imat <- solve(datamat, ...)
+  mat$setimat(imat)
+  imat      
 }
